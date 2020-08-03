552e7b10a12a0009bc17cab63748119b133f76fb
zgf <597119194@qq.com>
2020-08-03 15:35:22 +0800

    idea-spring

---
+++ src/main/java/cn/qhone/spring/American.java
@@ -0,0 +1,8 @@
+package cn.qhone.spring;
+
+public class American extends Person {
+
+    public void showinfo() {
+        System.out.println("听妈妈的话！！！");
+    }
+}
---
+++ src/main/java/cn/qhone/spring/Chinese.java
@@ -0,0 +1,8 @@
+package cn.qhone.spring;
+
+public class Chinese extends Person {
+
+    public void showinfo() {
+        System.out.println("听妈妈的话！！！");
+    }
+}
